2faa987a48e0e6e8bb64a101dbbf9ca87e79f0e1
Tommy Shi <tommyshi19991130@gmail.com>
2019-12-15 17:51:55 +1300

    Add to determine which player wins

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber
 
 var randomNumber2 = Math.round(Math.random() * 5 + 1);
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Draws!";
+}
